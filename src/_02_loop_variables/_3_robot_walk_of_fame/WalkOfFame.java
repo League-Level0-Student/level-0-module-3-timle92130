f47406ec4e80da3cce0d0d519c2b5ef35cc2961c
league <league@imac-12.attlocal.net>
2022-03-31 16:55:05 -0700

    99 bottles

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,18 +9,27 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(80);
+		rob.setSpeed(80000);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+		for (int x = 0; x < 10; x++) {
+		for (int i=0; i < 5; i++) {
+		rob.penDown();
+		rob.move(30);
+		rob.turn(144);
+		}
 		// 3. Set the length of each line in the star to 30.
-
+		rob.penUp();
+		rob.turn(90);
+		rob.move(50);
+		rob.turn(270);
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
-
+		}
 	}
 
 }
